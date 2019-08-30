ae46b47d772731e311d7640577cfae568210e997
Sascha Brandt <myeti@mail.uni-paderborn.de>
2019-08-30 11:19:32 +0200

    Allow http redirects for NetProvider

--- tests/NetProviderTest.cpp
+++ tests/NetProviderTest.cpp
@@ -7,22 +7,24 @@
 	You should have received a copy of the MPL along with this library; see the 
 	file LICENSE. If not, you can obtain one at http://mozilla.org/MPL/2.0/.
 */
-#include "IO/FileName.h"
-#include "IO/FileUtils.h"
-#include "StringUtils.h"
-#include "Utils.h"
-#include "Encoding.h"
+#include "../IO/FileName.h"
+#include "../IO/FileUtils.h"
+#include "../StringUtils.h"
+#include "../Utils.h"
+#include "../Encoding.h"
+#include "../IO/TemporaryDirectory.h"
 #include <catch2/catch.hpp>
 #include <cstdint>
 #include <cstdlib>
 #include <mutex>
 #include <vector>
+#include <iostream>
 
 const std::string testString1("Test12345678");
 const std::vector<uint8_t> byteString1(testString1.cbegin(), testString1.cend());
 	
 #ifdef UTIL_HAVE_LIB_CURL
-TEST_CASE("NetProviderTest_HTTP", "[NetProviderTest]") {	
+TEST_CASE("NetProviderTest_HTTP", "[NetProviderTest]") {
 	const Util::FileName fileName("http://httpbin.org/base64/" + Util::encodeBase64(byteString1));
 	
 	std::string content = Util::FileUtils::getFileContents(fileName);
@@ -42,6 +44,13 @@ TEST_CASE("NetProviderTest_FTP", "[NetProviderTest]") {
 	
 	std::string content = Util::FileUtils::getFileContents(fileName);
 	REQUIRE(md5.compare(Util::md5(content)) == 0);
+}
+
+TEST_CASE("NetProviderTest_Redirect", "[NetProviderTest]") {
+	const Util::FileName fileName("https://httpbin.org/redirect-to?url=https%3A%2F%2Fhttpbin.org%2Fbase64%2F" + Util::encodeBase64(byteString1));
+	
+	std::string content = Util::FileUtils::getFileContents(fileName);
+	REQUIRE(testString1.compare(content) == 0);
 }
 
 #endif
